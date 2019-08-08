82dcbb9471a3ae5c949951ef1063643a5da9ca46
Snir Vitrak Tamam <snirv@migal.org.il>
2019-08-08 18:02:32 +0300

    string formating, reduced lists

--- blockChain.py
+++ blockChain.py
@@ -1,4 +1,4 @@
-
+import functools
 # The reward we give to miners (for creating a new block)
 MINING_REWARD = 10
 
@@ -41,18 +41,17 @@ def get_balance(participant):
     # This fetches sent amounts of open transactions (to avoid double spending)
     open_tx_sender = [tx['amount'] for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
-    amount_sent = 0
+    print(tx_sender)
     # Calculate the total amount of coins sent
-    for tx in tx_sender:
-        if len(tx) > 0:
-            amount_sent += tx[0]
+    amount_sent = functools.reduce(lambda tx_sum, tx_amt: tx_sum+sum(tx_amt) if len(tx_amt) > tx_sum+0 else 0, tx_sender, 0)
+    
     # This fetches received coin amounts of transactions that were already included in blocks of the blockchain
     # We ignore open transactions here because you shouldn't be able to spend coins before the transaction was confirmed + included in a block
     tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant] for block in blockchain]
     amount_received = 0
     for tx in tx_recipient:
         if len(tx) > 0:
-            amount_received += tx[0]
+            amount_received += sum(tx)
     # Return the total balance
     return amount_received - amount_sent
 
@@ -216,7 +215,7 @@ while waiting_for_input:
         print('Invalid blockchain!')
         # Break out of the loop
         break
-    print(get_balance('Max'))
+    print('balance of {}: {:6.2f}'.format('max',get_balance('Max')))
 else:
     print('User left!')
 
